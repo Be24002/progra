40197df655e7d410af26171f038b5c634117312d
Be24002 <be24002@ues.edu.sv>
2024-09-10 15:14:41 -0600

    Create area, diametro y perimetro de un circulo.py

---
+++ area, diametro y perimetro de un circulo.py
@@ -0,0 +1,13 @@
+import math
+
+print(" determinar y mostrar el area, perimetro y diametro de un circulo")
+
+r= float(input("radio del circulo=\n"))
+pi= 3.14
+# operaciones
+
+A= pi*r**2
+P= 2*pi*r
+D= 2*r
+
+print(f" el rea el perimetro y el diametros son: {A},{P},{D}")
